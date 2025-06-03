dded96375ada9ff934b89b3b1d0ec1a0fc4b7bb9
Tom Chapman <tchapman000@gmail.com>
2025-06-02 22:30:06 -0700

    Restructured URIs

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/courses/info/{pid}", handlers.InfoHandler).Methods("GET")
-	r.HandleFunc("/api/courses/sections/{term}/{course}", handlers.SectionHandler).Methods("GET")
+	r.HandleFunc("/api/courses/{pid}/info", handlers.InfoHandler).Methods("GET")
+	r.HandleFunc("/api/courses/{course}/sections/{term}", handlers.SectionHandler).Methods("GET")
 	r.HandleFunc("/api/courses", handlers.CourseHandler).Methods("GET")
 
 	http.ListenAndServe(":8080", r)
