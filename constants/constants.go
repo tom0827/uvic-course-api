66ff3afa0db9d5c5e2de92d3eef24bb3e8184b5b
Tom Chapman <tchapman000@gmail.com>
2025-11-28 01:13:04 -0800

    updating kuali catalog ID

--- constants/constants.go
+++ constants/constants.go
@@ -1,10 +1,10 @@
 package constants
 
 // The Kuali catalog URL must be manually updated. This one is up to date as of 2025 June 2
-const CatalogUrl string = "https://uvic.kuali.co/api/v1/catalog/courses/67855445a0fe4e9a3f0baf82"
+const CatalogUrl string = "https://uvic.kuali.co/api/v1/catalog/courses/68923109c72beab1a99e4c73"
 
 // Kuali URL to get course information
-const InformationUrl string = "https://uvic.kuali.co/api/v1/catalog/course/67855445a0fe4e9a3f0baf82/%s"
+const InformationUrl string = "https://uvic.kuali.co/api/v1/catalog/course/68923109c72beab1a99e4c73/%s"
 
 const BaseUrl string = "https://banner.uvic.ca/StudentRegistrationSsb/ssb"
 const CookieUrl string = BaseUrl + "/classSearch/classSearch?term=%s&txt_subject=CSUP&txt_courseNumber=000"
